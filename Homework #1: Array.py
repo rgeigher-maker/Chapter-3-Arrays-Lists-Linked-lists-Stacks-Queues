eae7e1159308f335f565f7f805904dbabe42a2d9
rgeigher-maker <rgeigher@lions.molloy.edu>
2026-02-17 14:50:21 -0500

    Add files via upload

---
+++ Homework #1: Array.py
@@ -0,0 +1,12 @@
+# Step One: Initialization
+dataArray = [3, 6, 4, 8] #this line create the array with the given values [3, 6, 4, 8]
+print("Original Array:") #this line would print the array before adding addition numbers
+print(dataArray)
+print()
+
+# Step Two: Insertion
+dataArray.insert(0,7) #this line tells the code where to insert the 7. (0: the beginning position.)
+
+# Step Three: Finalization
+print("Updated Array:")
+print(dataArray) #this line would print the array after adding addition numbers
